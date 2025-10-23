04e513eac6dc031d6447195e42494c11105a5da0
akash-rawal <akashrawal444gmail.com>
2025-10-23 13:07:36 +0530

    minor fixes

--- frontend/invoice-generator/src/utils/apiPaths.js
+++ frontend/invoice-generator/src/utils/apiPaths.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:8000";
+// export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://ai-invoice-generator-ot1b.onrender.com";
 
 export const API_PATHS = {
   AUTH: {
